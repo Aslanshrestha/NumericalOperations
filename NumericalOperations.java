b3f3684e0aef537a2eec9762abfd28e78fe7cc62
Aslanshrestha <149399704+Aslanshrestha@users.noreply.github.com>
2024-09-03 11:40:28 -0600

    still working

--- NumericalOperations.java
+++ NumericalOperations.java
@@ -3,6 +3,9 @@
  */
 public class NumericalOperations{
     public static void main(String args[]){
-        //Complete code challenge 1 below
+        String p1= "Problem 1: -79 + 3 * (4 + 82 - 68) / 7 % 19 = " ;
+        double a1= -79 + 3 * (4 + 82 - 68) / 7 % 19 ;
+        String r1 = p1 + a1;
+        System.out.print= (r1);
     }
 }
