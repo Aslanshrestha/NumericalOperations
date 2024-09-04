4b02dd2c25eb9878c3019a911054df6f9a995783
Aslanshrestha <149399704+Aslanshrestha@users.noreply.github.com>
2024-09-04 16:10:29 -0600

    still working

--- NumReverse.java
+++ NumReverse.java
@@ -4,11 +4,38 @@
 
 public class NumReverse {
     public static void main(String args[]){
-        /*
-         * Implement your number reversal algorithm below
-         * Your code should work for any 4 digit integer.  
-         * When your code is ran the original and the 
-         * reversed number should print to the console.  
-         */
+        
+        int n1= 1234;
+        int lastNum1= n1%10;
+        System.out.print(lastNum1);
+        int thirdNum1=(n1/10)%10;
+        System.out.print(thirdNum1);
+        int secondNum1=(n1/100)%10;
+        System.out.print(secondNum1);
+        int firstNum1=(n1/1000)%10;
+        System.out.println(firstNum1);
+        
+
+        int n2= 6789;
+        int lastNum2= n2%10;
+        System.out.print(lastNum2);
+        int thirdNum2=(n2/10)%10;
+        System.out.print(thirdNum2);
+        int secondNum2=(n2/100)%10;
+        System.out.print(secondNum2);
+        int firstNum2=(n2/1000)%10;
+        System.out.println(firstNum2);
+       
+          
+        int n3= 1080;
+        int lastNum3= n3%10;
+        System.out.print(lastNum3);
+        int thirdNum3=(n3/10)%10;
+        System.out.print(thirdNum3);
+        int secondNum3=(n3/100)%10;
+        System.out.print(secondNum3);
+        int firstNum3=(n3/1000)%10;
+        System.out.println(firstNum3);
+        
     }
 }
--- NumericalOperations.java
+++ NumericalOperations.java
@@ -1,11 +1,24 @@
 /**
- * Indicate your first and last name along with a description of this class
+ * Aslan Shrestha
+ * This class description
+ * 
  */
 public class NumericalOperations{
     public static void main(String args[]){
         String p1= "Problem 1: -79 + 3 * (4 + 82 - 68) / 7 % 19 = " ;
         double a1= -79 + 3 * (4 + 82 - 68) / 7 % 19 ;
         String r1 = p1 + a1;
-        System.out.print= (r1);
+        System.out.println(r1);
+
+        String p2= "Problem 2: (179 + 21 +10)%7 / 181 * 2= ";
+        double a2= (179 + 21 +10)%7 / 181 * 2 ;
+        String r2= p2 + a2;
+        System.out.println(r2); 
+
+        String p3= "Problem 3: 10 / 2 * 2 + 10 % 2 =";
+        double a3= 10 / 2 * 2 + 10 % 2 ;
+        String r3=  p3 + a3;
+        System.out.println(r3);
+
     }
 }
